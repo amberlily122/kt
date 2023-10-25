8b9a545b4bf526d7d065403f9ba21fd7e0277647
Amber Adams <amber@vigilmd.com>
2023-10-24 22:23:11 -0700

    Add docs;simplify code
    Added doxygen comments.  Removed a complex-looking "if constexpr"
    tree and used function overloading for type-detection instead.

--- include/kt/concat.hpp
+++ include/kt/concat.hpp
@@ -1,35 +1,28 @@
 #ifndef concat_hpp_20230109_073634_PST
 #define concat_hpp_20230109_073634_PST
 #include <string>
-#include <valarray>
+#include <string_view>
 #include <sstream>
 namespace kt {
+/* \brief Convert a string to a string; this function exists for overloading purposes.
+*/
+auto to_string(const std::string& _src) -> std::string { return _src; }
+/* \brief Convert a string_view to a string; this function exists for overloading purposes.
+*/
+auto to_string(std::string_view _src)   -> std::string { return std::string(_src); }
+/* \brief Convert a c-string to a string; this function exists for overloading purposes. */
+auto to_string(const char* _src)        -> std::string { return _src; }
+/* \brief Convert a variable of type `_T` to a string as if it were sent to the 
+          formatted output operator of a std::stringstream object.
+*/
 template<typename _T>
-  auto to_string(_T&& _var)
+  auto to_string(_T&& _var)             -> std::string
   {
-    using var_type = std::decay_t<_T>;
-    if constexpr( std::is_same_v<var_type, std::string>
-               || std::is_same_v<var_type, std::string_view>
-               || std::is_same_v<var_type, const char*>
-                )
-    {
-      return _var;
-    }
-    else if constexpr(std::is_same_v<var_type, char>)
-    {
-      return std::string(1, _var);
-    }
-    else if constexpr(std::is_convertible_v<var_type, std::string>)
-    {
-      return static_cast<std::string>(_var);
-    }
-    else
-    {
-      auto ss = std::stringstream {};
-      ss << _var;
-      return std::move(ss.str());
-    }
-  };
+    return (std::stringstream {} << _var).str();
+  }
+/* \brief Convert each argument into a string, concatenate them, and return the result
+          as a new string.
+*/
 template<typename..._Ts>
   auto concat(_Ts&&..._vars) -> std::string
   {
