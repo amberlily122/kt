20ccaec9445158be99b2c9a8992e907de7035301
Amber Adams <amber@vigilmd.com>
2023-10-24 21:43:13 -0700

    documentation;new function optenv

--- include/kt/env.hpp
+++ include/kt/env.hpp
@@ -2,12 +2,21 @@
 #define env_hpp_20230301_144421_PST
 #include <cstdlib>
 #include <string_view>
+#include <optional>
 namespace kt {
- 
-auto getenv(std::string_view _n) -> std::string_view
+/* \brief Get the value of an environment variable as a string_view.
+*/ 
+inline auto getenv(const char* _n) -> std::string_view
   {
     auto e = std::getenv(_n);
     return e? e : "";
+  }
+/* \brief Get the value of an environment variable as an optional string_view.
+*/
+inline auto optenv(const char* _n) -> std::optional<std::string_view>
+  {
+    auto e = std::getenv(_n);
+    return e? e : std::nullopt;
   }
 } /* namespace kt */
 #endif//env_hpp_20230301_144421_PST
